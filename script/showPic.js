616d5b328889df5c25598e67c2d86026cf0cb99e
949nb <1661346771@qq.com>
2019-02-02 00:30:45 +0800

    1

---
+++ script/showPic.js
@@ -0,0 +1,15 @@
+//把<a>标签中的href 赋值到 <img>的src上。
+function showPic(whichpic) {
+    var source = whichpic.getAttribute("href")
+    var placeholder = document.getElementById("placeholder")
+    placeholder.setAttribute("src",source);
+}
+//把body中的元素的个数打印到屏幕上
+function countBodyChildren(){
+    var body_element = document.getElementsByTagName("body")[0];
+    console.log(body_element.childNodes.length);
+}
+//在onload（页面加载时）执行函数countBodyChildren。
+window.onload = countBodyChildren;
+
+//把<p>标签中的文字替换成<a>中的title。
